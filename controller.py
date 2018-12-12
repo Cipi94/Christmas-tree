d5d97c1d89230d50304ae5c984824a965221ddb6
Luca-Colombo <lucacolo94@gmail.com>
2018-12-13 00:21:27 +0100

    Added controller Singleton

--- controller.py
+++ controller.py
@@ -1,12 +1,25 @@
 from xmas import Xmas
+from acceso import Acceso
 import threading
 
 
 class Controller:
+    _instance = None
+
+    @staticmethod
+    def getInstance():
+        if Controller._instance is None:
+            Controller()
+        return Controller._instance
 
     def __init__(self):
-        self._t1 = threading.Thread()
-        self._xmas = Xmas()
+        if Controller._instance is not None:
+            raise Exception("This is a singleton class! Use get getInstance")
+        else:
+            Controller._instance = self
+            self._t1 = threading.Thread()
+            self._xmas = Xmas()
+            self._acceso = Acceso()
 
     def __init_thread(self, music, is_vixen):
         if is_vixen:
@@ -23,3 +36,12 @@ class Controller:
 
     def stop(self):
         self._xmas.stop_music()
+
+    def turn_on(self):
+        if self._t1.isAlive():
+            raise ValueError("Thread already started")
+        else:
+            self._t1 = threading.Thread(target=self._acceso.on())
+
+    def turn_off(self):
+        self._acceso.off()
