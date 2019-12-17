2ab32dc2b4ab3b1ff4e229127ec33ebb6b6be999
Luca-Colombo <lucacolo94@gmail.com>
2019-12-18 00:25:58 +0100

    converted all old songs to vixen csv format

--- songEncoding/converter.py
+++ songEncoding/converter.py
@@ -1,18 +1,20 @@
 import sys
 
+# Convert old manual-encoded songs to vixen csv export format
+
 ON = 255
 OFF = 0
 
-with open("./jinglebells.csv", 'r') as f:
-    with open("jingleNEW.csv", 'w') as f1:
+with open(sys.argv[1], 'r') as f:
+    with open(sys.argv[2], 'w') as f1:
         seq_data = f.readlines()
-        newRow = [0] * 7
+        newRow = ["0"] * 7
         printLines = []
-        for i in range(len(seq_data)):
+        for i in range(1,len(seq_data)):
             origRow = seq_data[i].rstrip().split(",")
             #print(origRow)
             time = origRow[0]
-            if newRow[0] != time:
+            if newRow[0] != time: # if the time of the new line changes we have completed converting the current millis so we write the line to the new file and continue with a new empty array for the new millis
                 #print(newRow)
                 printLine = ''
                 for col in newRow:
